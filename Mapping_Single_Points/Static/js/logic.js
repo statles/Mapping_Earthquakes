7d3ee78c880c38bdd677880eff78dafec3769148
statles <alex.statley@gmail.com>
2022-03-17 20:06:14 -0700

    updating map with circles and markers

--- Mapping_Single_Points/Static/js/logic.js
+++ Mapping_Single_Points/Static/js/logic.js
@@ -3,14 +3,30 @@ console.log("working");
 
 //initialize map
 //set coordinates and zoom level
-var map = L.map('mapid').setView([34.0522, -118.2437], 13);
+var map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(map);
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
 
 //markers, circles, and polygons
 //add marker to the map for Los Angeles, California
-var marker = L.marker([34.0522, -118.2437]).addTo(map);
+//var marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//add a circle to the map
+//use circle() or circlerMarker()
+
+L.circleMarker([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: 'yellow',
+    fillOpacity: 0.2,
+    //change the radius to make the circle bigger or smaller
+    //the radius is measured in meters
+    radius: 300
+}).addTo(map);
